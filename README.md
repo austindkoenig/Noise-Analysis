95c8bfbd912ff88cb9cae9f0b86b8bd23d8597d1
Austin Koenig <austindkoenig@gmail.com>
2019-10-07 12:12:06 -0700

    Completes data generation, preprocessing, and saving.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# A Brief Noise Analysis
+# <center>A Brief Noise Analysis</center>
 
-
+<center>Austin Koenig</center>
 
 ## Data
 
-- Domain: $N$ uniformly spaced points in $[-4\pi, 4\pi]$
+- Domain: $N$ uniformly spaced points in $[-m\pi, m\pi]$ for $m\in\mathbb{Z}$.
 - Training Data: Random samples from normal distribution with mean $\sin{(domain)}$ and standard deviation $s^2\in \{ 0.00001, 0.001, 0.1 \}$.
 - Testing Data: Exact values of $\sin{(domain)}$.
 
--- koenig-noise.py
+++ koenig-noise.py
@@ -1,26 +1,112 @@
-# Noise Study
+# Noise Analysis
 # Austin Koenig
+
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+import joblib
+import os
+import shutil
 
 np.random.seed(12)
 
+#plt.rc('text', usetex=True)
+#plt.rc('font', family='serif')
 
-def prepare_data(N = 500, 
-                domain = (-2 * np.pi, 2 * np.pi),
-                noise = [0.0001, 0.01, 0.1]):
-    x = np.linspace(domain[0], domain[1], N)
-    rawy = np.sin(x)
-    y = [np.random.normal(rawy, n) for n in noise]
-    plt.plot(x, rawy, '-', label = "True Sine")
-    for i in range(len(y)):
-        plt.plot(x, y[i], '.', label = f"Noise {noise[i]}", markersize = 3)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("Superposition of Noisy Sine onto Sine")
-    plt.legend()
-    plt.show()
+def check_dirs():
+    dirs = ['./files/', './files/figures/', './files/data/', './files/scalers/', './files/models/']
+    for d in dirs:
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+def clean_up():
+    shutil.rmtree('./files/')
+
+def generate_data(N = 1000, M = 2, deg_noise = 3):
+    # GENERATE DATA
+    noise = np.geomspace(10 ** -3, 10 ** -1, deg_noise)
+    domain = np.linspace(-M * np.pi, M * np.pi, N)
+    tsine = np.sin(domain)
+    nsine = np.array([np.random.normal(tsine, n) for n in noise]).T
+
+    # VISUALIZE DATA
+    # figure = plt.figure(figsize = (20, 20))
+    # a1 = figure.add_subplot(len(noise) + 1, 1, 1)
+    # a1.plot(domain, tsine, 'k-', label = "True Sine")
+    # a1.title.set_text("Test Set")
+    # a1.legend()
+
+    # if not os.path.exists('./figures/'):
+    #     os.mkdir('./figures/')
+    # #test_figure.savefig('./figures/test_figure.pdf')
+
+    # #train_figure = plt.figure(figsize = (20, 20))
+    
+    # axes = []
+    # for i in range(1, len(nsine) + 1):
+    #     axes.append(figure.add_subplot(len(noise), 1, i + 1))
+    #     axes[i - 1].plot(domain, nsine[:, i - 1], 'ko', label = f"Noise: {noise[i - 1]}", markersize = 2)
+    #     axes[i - 1].title.set_text(f"Training Set {i}")
+    #     axes[i - 1].legend()
+    
+    # #train_figure.savefig('./figures/train_figure.pdf')
+    # plt.show()
+
+    # SPLIT DATA
+    tr_rat = 0.6
+    val_rat = 0.2
+    ts_rat = 0.2
+    tr_split = round(tr_rat * tsine.shape[0])
+    val_split = round(val_rat * tsine.shape[0])
+    ts_split = round(ts_rat * tsine.shape[0])
+
+    splits = {
+        'train': (tr_split, tr_rat),
+        'validation': (val_split, val_rat),
+        'test': (ts_split, ts_rat)
+    }
+
+    unscaled_data = {
+        'tr': {
+            'x': domain[:tr_split],
+            'y': nsine[:tr_split, :]
+        },
+
+        'val': {
+            'x': domain[tr_split:(tr_split + val_split)],
+            'y': nsine[tr_split:(tr_split + val_split), :]
+        }, 
+
+        'ts': {
+            'x': domain[(tr_split + val_split):],
+            'y': nsine[(tr_split + val_split):, :]
+        }
+    }
+
+    # SCALE DATA
+    scaler = MinMaxScaler()
+    scaled_data = {
+        'tr': {
+            'x': unscaled_data['tr']['x'],
+            'y': scaler.fit_transform(unscaled_data['tr']['y'])
+        },
+
+        'val': {
+            'x': unscaled_data['val']['x'],
+            'y': scaler.transform(unscaled_data['val']['y'])
+        }, 
+
+        'ts': {
+            'x': unscaled_data['ts']['x'],
+            'y': scaler.transform(unscaled_data['ts']['y'])
+        }
+    }
+    
+    joblib.dump(splits, './files/data/splits')
+    joblib.dump(unscaled_data, './files/data/unscaled_data')
+    joblib.dump(scaled_data, './files/data/scaled_data')    
 
 def poly_interp():
     pass
@@ -31,10 +117,20 @@ def conv1d():
 def rnn():
     pass
 
-def evaluation(model):
+def evaluation():
     pass
 
 
 if __name__ == "__main__":
     print("----NOISE STUDY----")
-    prepare_data()
+    clean = True
+    print("Creating directories...")
+    check_dirs()
+    print("Directories created.")
+    print("Generating data...")
+    generate_data()
+    print("Data generated.")
+    if clean: 
+        print("Cleaning up...")
+        clean_up()
+        print("All cleaned up.")
